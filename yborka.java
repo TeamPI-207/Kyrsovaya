e3d6475b1ff926edd3c1b5a0844aef18644145ca
IlyaKostarev <ilushka-777@mail.ru>
2014-04-05 11:24:16 +0600

    Create yborka.java

---
+++ yborka.java
@@ -0,0 +1,366 @@
+Package javaapplication1; 
+public class yborka extends javax.swing.JFrame 
+{
+initComponents();
+}
+@SuppressWarnings("unchecked")
+// <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+private void initComponents()
+{
+jPanel1 = new javax.swing.JPanel();
+jTextField1 = new javax.swing.JTextField();
+jTextField2 = new javax.swing.JTextField(); 
+jLabel1 = new javax.swing.JLabel();
+jButton1 = new javax.swing.JButton();
+jButton2 = new javax.swing.JButton();
+jButton3 = new javax.swing.JButton();
+jButton4 = new javax.swing.JButton();
+jButton5 = new javax.swing.JButton();
+jButton6 = new javax.swing.JButton();
+jButton7 = new javax.swing.JButton();
+jButton8 = new javax.swing.JButton();
+jButton9 = new javax.swing.JButton();
+jButton10 = new javax.swing.JButton();
+jButton11 = new javax.swing.JButton();
+jButton12 = new javax.swing.JButton();
+
+setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+ 
+jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Уборка"));
+ 
+jLabel1.setText("Стоимость:"); 
+ 
+jButton1.setText("1");
+jButton1.addActionListener(new java.awt.event.ActionListener()
+{ 
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{ 
+jButton1ActionPerformed(evt);
+} 
+}); 
+
+jButton2.setText("2");
+jButton2.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton2ActionPerformed(evt);
+}
+});
+jButton3.setText("3"); 
+jButton3.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton3ActionPerformed(evt);
+}
+});
+ 
+jButton4.setText("4");
+jButton4.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton4ActionPerformed(evt); 
+} 
+}); 
+ 
+jButton5.setText("5");
+jButton5.addActionListener(new java.awt.event.ActionListener()
+ {
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton5ActionPerformed(evt);
+}
+});
+ 
+jButton6.setText("6");
+jButton6.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton6ActionPerformed(evt);
+}
+});
+jButton7.setText("7");
+jButton7.addActionListener(new java.awt.event.ActionListener()
+{ public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton7ActionPerformed(evt);
+}
+});  /* завершение создания  "слушателя" для кнопки 7*/
+ 
+jButton8.setText("8");
+jButton8.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{  
+jButton8ActionPerformed(evt);
+}
+}); 
+ 
+jButton9.setText("9");
+jButton9.addActionListener(new java.awt.event.ActionListener()
+{
+public void actionPerformed(java.awt.event.ActionEvent evt)
+{
+jButton9ActionPerformed(evt);
+}
+});
+ 
+jButton10.setText("0");
+jButton10.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton10ActionPerformed(evt); 
+}
+});
+ 
+ jButton11.setText("."); 
+jButton11.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt)
+{
+jButton11ActionPerformed(evt);
+}
+});
+ 
+jButton12.setText("OK");
+jButton12.addActionListener(new java.awt.event.ActionListener() 
+{
+public void actionPerformed(java.awt.event.ActionEvent evt) 
+{
+jButton12ActionPerformed(evt);
+}
+});
+ 
+javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+jPanel1.setLayout(jPanel1Layout);
+jPanel1Layout.setHorizontalGroup( 
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING) 
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addContainerGap()
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+.addComponent(jButton11, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,Short.MAX_VALUE)
+.addComponent(jButton7, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE))
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+.addComponent(jButton10, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
+.addComponent(jButton8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addComponent(jButton12, javax.swing.GroupLayout.Alignment.TRAILING)))
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addComponent(jLabel1)
+.addGap(18, 18, 18)
+.addComponent(jTextField2))))
+);
+jPanel1Layout.setVerticalGroup(
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addGap(7, 7, 7)
+.addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jLabel1)
+.addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+.addGap(36, 36, 36)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jButton2)
+.addComponent(jButton1)
+.addComponent(jButton3))
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jButton4)
+.addComponent(jButton5)
+.addComponent(jButton6))
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jButton7)
+.addComponent(jButton8)
+.addComponent(jButton9))
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jButton10)
+.addComponent(jButton11)
+.addComponent(jButton12))
+.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+);
+
+javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+getContentPane().setLayout(layout);
+layout.setHorizontalGroup(
+layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+layout.setVerticalGroup(
+layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+ pack();
+}// </editor-fold>//GEN-END:initComponents 
+private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton1ActionPerformed
+jTextField2.setText(jTextField2.getText() + 1);// TODO add your handling code here:
+}
+//GEN-LAST:event_jButton1ActionPerformed 
+private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton2ActionPerformed
+jTextField2.setText(jTextField2.getText() + 2); // TODO add your handling code here: 
+}
+//GEN-LAST:event_jButton2ActionPerformed
+ivate void jButton3ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton3ActionPerformed
+jTextField2.setText(jTextField2.getText() + 3);// TODO add your handling code here: 
+}
+//GEN-LAST:event_jButton3ActionPerformed
+private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton4ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 4);// TODO add your handling code here:  
+
+}
+//GEN-LAST:event_jButton4ActionPerformed
+ 
+private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton5ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 5);// TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton5ActionPerformed
+ 
+private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton6ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 6); // TODO add your handling code here: 
+
+}//GEN-LAST:event_jButton6ActionPerformed
+ 
+private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton7ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 7); // TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton7ActionPerformed
+ 
+private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton8ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 8);// TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton8ActionPerformed
+ 
+private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton9ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 9);// TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton9ActionPerformed
+ 
+private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) 
+{
+//GEN-FIRST:event_jButton11ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + "."); // TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton11ActionPerformed
+
+private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
+
+jTextField2.setText(jTextField2.getText() + 0);// TODO add your handling code here: 
+
+}
+//GEN-LAST:event_jButton10ActionPerformed
+ 
+private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
+
+jTextField1.setText(jTextField2.getText());
+
+JavaApplication1.setLast_result(Float.parseFloat(jTextField2.getText()));// TODO add your handling code here:
+}
+//GEN-LAST:event_jButton12ActionPerformed
+public static void main(String args[]) 
+{ 
+//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+try 
+{ 
+for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) 
+{
+if ("Nimbus".equals(info.getName())) 
+{
+javax.swing.UIManager.setLookAndFeel(info.getClassName()); 
+break; 
+}
+} 
+} catch (ClassNotFoundException ex)
+{ 
+java.util.logging.Logger.getLogger(yborka.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} catch (InstantiationException ex) 
+{ 
+java.util.logging.Logger.getLogger(yborka.class.getName()).log(java.util.logging.Level.SEVERE, null, ex); 
+}
+ catch (IllegalAccessException ex) 
+{ 
+java.util.logging.Logger.getLogger(yborka.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} 
+catch (javax.swing.UnsupportedLookAndFeelException ex) 
+{
+            java.util.logging.Logger.getLogger(yborka.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} 
+//</editor-fold>
+java.awt.EventQueue.invokeLater(new Runnable() 
+{
+public void run() 
+{ 
+new yborka().setVisible(true);
+}
+}); 
+}
+// Variables declaration - do not modify//GEN-BEGIN:variables
+private javax.swing.JButton jButton1;
+private javax.swing.JButton jButton10;
+private javax.swing.JButton jButton11;
+private javax.swing.JButton jButton12;
+private javax.swing.JButton jButton2;
+private javax.swing.JButton jButton3;
+private javax.swing.JButton jButton4;
+private javax.swing.JButton jButton5;
+private javax.swing.JButton jButton6;
+private javax.swing.JButton jButton7;
+private javax.swing.JButton jButton8;
+private javax.swing.JButton jButton9;
+private javax.swing.JLabel jLabel1;
+private javax.swing.JPanel jPanel1; 
+private javax.swing.JTextField jTextField1;
+private javax.swing.JTextField jTextField2;
+// End of variables declaration//GEN-END:variables
+}
